700958fcdf67863dcf35f4dd946e3de2b5b2219e
Irem Sen <secilreel@yahoo.com>
2019-04-09 16:38:45 -0400

    working on last question

--- drills.js
+++ drills.js
@@ -1,8 +1,7 @@
-'use strict';
-const BinarySearchTree = require('./BST');
+"use strict";
+const BinarySearchTree = require("./BST");
 
-
-function main(){
+function main() {
   const BST = new BinarySearchTree();
   BST.insert(3, 3);
   BST.insert(1, 1);
@@ -27,13 +26,13 @@ function main(){
   // BST.insert('N', 'N');
 
   //console.log(BST);
-  //console.log(height(BST))
+  //   console.log(height(BST));
   //console.log(isItBST(BST))
-  console.log(findThrid(BST))
+  //   console.log(findThrid(BST));
+  console.log(isBalanced(BST));
 }
 
 main();
-
 
 // 4. What does this program do?
 // The function adds up all values in the tree.
@@ -41,52 +40,51 @@ main();
 
 // 5. Height of BST
 
-
-function height(tree){
-  if(tree === null){
+function height(tree) {
+  if (tree === null) {
     return 0;
   }
   let rightCount = height(tree.right);
+  console.log("right", rightCount);
   let leftCount = height(tree.left);
-  return 1+Math.max(rightCount, leftCount);
+  console.log("left", leftCount);
+  return 1 + Math.max(rightCount, leftCount);
 }
 
 // 6. Is it a BST?
 
-function isItBST(tree){
-
-  if(
-    tree.left > tree || 
-    tree.left > tree.right || 
-    tree.right < tree || 
-    tree.right < tree.left){
+function isItBST(tree) {
+  if (
+    tree.left > tree ||
+    tree.left > tree.right ||
+    tree.right < tree ||
+    tree.right < tree.left
+  ) {
     return false;
   }
-  if(!tree.right && !tree.left){
-    if(!tree.parent){
+  if (!tree.right && !tree.left) {
+    if (!tree.parent) {
       return false;
-    }
-    else return;
+    } else return;
   }
-  if(tree.left){
+  if (tree.left) {
     isItBST(tree.left);
   }
-  if(tree.right){
+  if (tree.right) {
     isItBST(tree.right);
   }
   return true;
-
 }
 
 // 7. 3rd largest node
 function findThrid(tree) {
   // find the largest node(to the right)
   if (!tree.right) {
-    if(tree.left !== null){
+    if (tree.left !== null) {
       return tree.parent;
     }
-    if(!tree.left){
-      if(!tree.parent){
+    if (!tree.left) {
+      if (!tree.parent) {
         return tree;
       }
       return tree.parent.left;
@@ -95,4 +93,22 @@ function findThrid(tree) {
   return findThrid(tree.right);
 }
 
+function isBalanced(tree) {
+  if (tree === null) {
+    return 0;
+  }
 
+  let rightCount = height(tree.right);
+  let leftCount = height(tree.left);
+  let result = Math.abs(rightCount - leftCount);
+  return (result > 1)? false: true;
+}
+
+function areTheySame(arr1, arr2){
+  if(arr1 === arr2) true;
+  if(arr1.length !== arr2.length) false;
+  if(arr1[0] !== arr2[0]) false;
+
+
+
+}
